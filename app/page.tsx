94d375b9877b66b1a5ba798374576e9a1d058424
Leonid Medovyy <leonid@macron1.io>
2024-12-29 13:27:00 -0700

    setting up chords bank

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import ChordBank from "@/components/ChordBank";
+
 export default function Home() {
   return (
       <div>
-        <h1 className={"font-mono"}>Initial Setup..</h1>
-        <h2 className={"font-sans"}>Initial Setup..</h2>
+        <ChordBank />
       </div>
   );
 }
