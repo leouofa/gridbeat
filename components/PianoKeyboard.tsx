cda4843b077499faf211aafc9a3f680ddcaf5eba
Leonid Medovyy <leonid@macron1.io>
2024-12-30 21:12:57 -0700

    modified the keyboard to highlight the keys

--- components/PianoKeyboard.tsx
+++ components/PianoKeyboard.tsx
@@ -1,54 +1,47 @@
 import React from "react";
+import { NOTES } from "@/constants";
+import { Note } from "@/types";
 
 interface PianoKeyProps {
-  isBlack: boolean;
-  note: string;
+  note: Note;
+  octave: number;
 }
 
-const PianoKey: React.FC<PianoKeyProps> = ({ isBlack, note }) => {
+const PianoKey: React.FC<PianoKeyProps> = ({ note, octave }) => {
+  const isBlack = !note.natural;
+
   return (
     <div
       className={`
-            ${
-              isBlack
-                ? "bg-black h-32 w-8 -mx-4 z-10 hover:bg-gray-800"
-                : "bg-white h-48 w-12 hover:bg-gray-100"
-            }
-            relative cursor-pointer border border-gray-800
-          `}
-      data-note={note}
-    />
+        ${isBlack ? "bg-black h-32 w-8 -mx-4 z-10 pl-1 pt-16" : `bg-white h-48 w-12 pl-5 pt-36 `}
+        relative border border-gray-800 font-mono
+      `}
+      data-note={`${note.name}${octave}`}
+      style={{
+        background: isBlack
+          ? `linear-gradient(to bottom, black 0%, black 30%, ${note.color} 30%, ${note.color} 100%)`
+          : note.color,
+        color: note.textColor,
+      }}
+    >
+      {note.name}
+    </div>
   );
 };
 
 const PianoKeyboard: React.FC = () => {
   const createKeys = () => {
-    const notes = [
-      "C",
-      "C#",
-      "D",
-      "D#",
-      "E",
-      "F",
-      "F#",
-      "G",
-      "G#",
-      "A",
-      "A#",
-      "B",
-    ];
     const keys = [];
-    const octaves = 2; // Full piano has 7 octaves plus a few extra keys
+    const octaves = 2;
 
     for (let octave = 0; octave < octaves; octave++) {
-      notes.forEach((note) => {
-        const isBlack = note.includes("#");
-        keys.push({ note: `${note}${octave}`, isBlack });
+      NOTES.forEach((note) => {
+        keys.push({ note, octave });
       });
     }
 
     // Add the final C key for the last octave
-    keys.push({ note: "C8", isBlack: false });
+    keys.push({ note: NOTES[0], octave: 2 });
 
     return keys;
   };
@@ -59,7 +52,7 @@ const PianoKeyboard: React.FC = () => {
     <div className="w-full overflow-x-auto p-4">
       <div className="flex relative">
         {keys.map((key, index) => (
-          <PianoKey key={index} isBlack={key.isBlack} note={key.note} />
+          <PianoKey key={index} note={key.note} octave={key.octave} />
         ))}
       </div>
     </div>
